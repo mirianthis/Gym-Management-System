23ea68dd2f09090d16e70b5b75dcd5cf48fef17c
Mirianthis Apostolidis <mirianthis2000@gmail.com>
2025-01-16 00:36:20 +0200

    Dashboard page implemented

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,43 +1,83 @@
 import { Component } from '@angular/core';
-import ApexCharts from 'apexcharts';
+import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCoffee, faSync } from '@fortawesome/free-solid-svg-icons';
+import { relative } from 'path';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CanvasJSAngularChartsModule, FontAwesomeModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-  public newMembersChart: any;
-  public revenueChart: any;
-  constructor() {
-    this.newMembersChart = {
-      // Data: Data to be displayed in the chart
-      data: [
-        { month: 'Jan', avgTemp: 2.3, iceCreamSales: 162000 },
-        { month: 'Mar', avgTemp: 6.3, iceCreamSales: 302000 },
-        { month: 'May', avgTemp: 16.2, iceCreamSales: 800000 },
-        { month: 'Jul', avgTemp: 22.8, iceCreamSales: 1254000 },
-        { month: 'Sep', avgTemp: 14.5, iceCreamSales: 950000 },
-        { month: 'Nov', avgTemp: 8.9, iceCreamSales: 200000 },
-      ],
-      // Series: Defines which chart type and data to use
-      series: [{ type: 'bar', xKey: 'month', yKey: 'iceCreamSales' }]
-    };
+  public attendanceReportChart: any;
+  public totalReportsChart: any;
 
-    this.revenueChart = {
-      // Data: Data to be displayed in the chart
-      data: [
-        { month: 'Jan', avgTemp: 2.3, iceCreamSales: 162000 },
-        { month: 'Mar', avgTemp: 6.3, iceCreamSales: 302000 },
-        { month: 'May', avgTemp: 16.2, iceCreamSales: 800000 },
-        { month: 'Jul', avgTemp: 22.8, iceCreamSales: 1254000 },
-        { month: 'Sep', avgTemp: 14.5, iceCreamSales: 950000 },
-        { month: 'Nov', avgTemp: 8.9, iceCreamSales: 200000 },
-      ],
-      // Series: Defines which chart type and data to use
-      series: [{ type: 'line', xKey: 'month', yKey: 'iceCreamSales' }]
-    };
+  faSync = faSync;
+  constructor() {
+
+    this.totalReportsChart = {
+      animationEnabled: true,
+      fontSize: 10,
+      title: {
+        text: "Total Reports"
+      },
+      data: [{
+        type: "doughnut",
+        yValueFormatString: "#,###.##'%'",
+        indexLabel: "{name}",
+        dataPoints: [
+          { y: 28, name: "Members" },
+          { y: 10, name: "Trainer" },
+          { y: 20, name: "Equipment" },
+          { y: 15, name: "Revenue" }
+        ]
+      }]
+    }
+
+    this.attendanceReportChart = {
+      animationEnabled: true,
+      title: {
+        text: "Attendance Reports"
+      },
+      axisX:{
+        lineThickness: 0,
+        tickThickness: 0,
+        labelFontSize: 0
+      },
+      axisY:{
+        lineThickness: 0,
+        gridThickness: 0,
+        tickLength: 0,
+        labelFontSize: 0
+      },
+      data: [{
+        type: "column",
+        color: "#344CB7",
+        dataPoints: [
+          { label: "Monday", y: 22 },
+          { label: "Tuesday", y: 18 },
+          { label: "Wednesday", y: 23 },
+          { label: "Thursday", y: 25 },
+          { label: "Friday", y: 13 },
+          { label: "Saturday", y: 15 },
+          { label: "Sunday", y: 6 },
+        ]
+      }, {
+        type: "column",
+        color: "#85A947",
+        dataPoints: [
+          { label: "Monday", y: 17 },
+          { label: "Tuesday", y: 10 },
+          { label: "Wednesday", y: 23 },
+          { label: "Thursday", y: 26 },
+          { label: "Friday", y: 12 },
+          { label: "Saturday", y: 17 },
+          { label: "Sunday", y: 3 },
+        ]
+      }]
+    }
   }
 }
