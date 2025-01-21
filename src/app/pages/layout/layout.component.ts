33cc8ae9eea1af4abdc6e3f9d1c78028da2502c8
Mirianthis Apostolidis <mirianthis2000@gmail.com>
2025-01-21 22:57:23 +0200

    Fixed Sidebar and Navbar

--- src/app/pages/layout/layout.component.ts
+++ src/app/pages/layout/layout.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+// layout.component.ts
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { LoginService } from '../../services/login.service';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit{
 
+  constructor(public loginService: LoginService) {}
+
+  ngOnInit(): void {
+    this.userName = this.loginService.userName;
+  }
+
+  sidebarActive: boolean = false;
+  activeItem: string = '';
+  userName: string = '';
+
+  toggleSidebar(): void {
+    this.sidebarActive = !this.sidebarActive;
+  }
+
+  isActive(item: string): boolean {
+    return this.activeItem === item;
+  }
+
+  setActive(item: string): void {
+    this.activeItem = item;
+  }
 }
