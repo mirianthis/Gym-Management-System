0dc9674744bbfab6836ee23c26c33a10ea20f011
Mirianthis Apostolidis <mirianthis2000@gmail.com>
2025-01-31 00:28:18 +0200

    Added Modal for category and installment plan

--- src/app/pages/membership-list/add-membership/add-membership.component.ts
+++ src/app/pages/membership-list/add-membership/add-membership.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-add-membership',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './add-membership.component.html',
   styleUrl: './add-membership.component.css'
 })
