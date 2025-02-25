6a28877f543dd8eb82fe7223d936792b35d6a248
Mirianthis Apostolidis <mirianthis2000@gmail.com>
2025-02-26 00:18:31 +0200

    Finished settings

--- Frontend/src/app/models/settings.model.ts
+++ Frontend/src/app/models/settings.model.ts
@@ -4,8 +4,8 @@ export class GymSettings {
   starting_year: number;
   gym_address: string;
   office_phone_number: string;
-  country: string;
-  system_language: string;
+  country?: string;
+  system_language?: string;
   email: string;
   gym_logo?: File | string;
   cover_image?: File | string;
@@ -15,23 +15,23 @@ export class GymSettings {
   waist_unit: string;
   arms_unit: string;
   fat_unit: string;
-  member_can_view_others: boolean;
-  staff_can_view_trainees: boolean;
-  enable_alert_email: boolean;
+  member_can_view_others?: boolean;
+  staff_can_view_trainees?: boolean;
+  enable_alert_email?: boolean;
   reminder_before_days?: number;
   reminder_message?: string;
-  members_can_message_each_other: boolean;
+  members_can_message_each_other?: boolean;
   left_header_text?: string;
   footer_text?: string;
 
   constructor(data: Partial<GymSettings> = {}) {
     this.id = data.id;
     this.gym_name = data.gym_name || '';
-    this.starting_year = data.starting_year || new Date().getFullYear();
+    this.starting_year = data.starting_year!;
     this.gym_address = data.gym_address || '';
     this.office_phone_number = data.office_phone_number || '';
-    this.country = data.country || '';
-    this.system_language = data.system_language || 'English';
+    this.country = data.country;
+    this.system_language = data.system_language;
     this.email = data.email || '';
     this.gym_logo = data.gym_logo;
     this.cover_image = data.cover_image;
@@ -41,12 +41,12 @@ export class GymSettings {
     this.waist_unit = data.waist_unit || 'cm';
     this.arms_unit = data.arms_unit || 'cm';
     this.fat_unit = data.fat_unit || '%';
-    this.member_can_view_others = data.member_can_view_others || false;
-    this.staff_can_view_trainees = data.staff_can_view_trainees || false;
-    this.enable_alert_email = data.enable_alert_email || false;
+    this.member_can_view_others = data.member_can_view_others;
+    this.staff_can_view_trainees = data.staff_can_view_trainees;
+    this.enable_alert_email = data.enable_alert_email;
     this.reminder_before_days = data.reminder_before_days;
     this.reminder_message = data.reminder_message;
-    this.members_can_message_each_other = data.members_can_message_each_other || false;
+    this.members_can_message_each_other = data.members_can_message_each_other;
     this.left_header_text = data.left_header_text;
     this.footer_text = data.footer_text;
   }
--- Frontend/src/app/pages/settings/settings.component.ts
+++ Frontend/src/app/pages/settings/settings.component.ts
@@ -4,51 +4,73 @@ import { GymSettings } from '../../models/settings.model';
 import { SettingsService } from '../../services/settings-service/settings.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Router, RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
+import { ToastModule } from 'primeng/toast';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterLink],
+  imports: [CommonModule, FormsModule, ToastModule],
+  providers: [MessageService],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.css'
 })
 export class SettingsComponent {
-  settings$?: Observable<any>;
 
   newSettings = new GymSettings();
 
+  settingsExist: boolean = false;
 
-  constructor(private router: Router, private settingsService: SettingsService) { }
+
+  constructor(private router: Router, private settingsService: SettingsService, private messageService: MessageService) { }
 
 
   ngOnInit(): void {
-    this.settings$ = this.settingsService.getSettings();
+
+    this.settingsService.getCheckSettings().subscribe(data => {
+      if (data.exists) {
+
+        // If settings exist, fetch them and populate newSettings
+        this.settingsService.getSettings().subscribe(settings => {
+          this.newSettings = new GymSettings(settings); // Fill form with existing values
+        });
+
+        this.settingsExist = true;
+      } else {
+
+        // No settings exist, keep default newSettings
+        console.log('No existing settings found. Using default values.');
+      }
+    });
   }
 
-  onCancel() {
-    this.router.navigate(['/dashboard']);
+  onClear() {
+    this.newSettings = new GymSettings();
   }
 
 
-  onSave(settingsId: number, settingsData: any) {
+  onSave(settingsData: any) {
     this.settingsService.getCheckSettings().subscribe(data => {
       if (data.exists) {
         this.settingsService.updateSettings(data.settings_id, settingsData).subscribe(data => {
           console.log('Settings updated successfully:', data);
-          this.router.navigate(['/dashboard']);
+          this.messageService.add({severity: 'success', summary:  'Success', detail: 'Settings updated successfully' });
         },
           error => {
             console.error('Error updating settings:', error);
+            this.messageService.add({severity: 'error', summary:  'Error', detail: 'Error updating settings' });
           }
         );
       } else {
         this.settingsService.addSettings(settingsData).subscribe(data => {
           console.log('Settings added successfully:', data);
-          this.router.navigate(['/dashboard']);
+          this.messageService.add({severity: 'success', summary:  'Success', detail: 'Settings added successfully' });
         },
           error => {
             console.error('Error adding settings:', error);
+            this.messageService.add({severity: 'error', summary:  'Error', detail: 'Error adding settings' });
           }
         );
       }
